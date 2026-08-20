d57d8120859b54975cc5a6233e12230e312efd67
apoorv-gthub <aal.apoorv@gmail.com>
2026-08-20 22:49:53 +0530

    Average of Elements in array

---
+++ avgOfElements.java
@@ -0,0 +1,17 @@
+package dsaArrays;
+
+public class avgOfElements {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		int arr[] = {11,22,33,44,55};
+		
+		for(int i = 0; i< arr.length; i++) {
+			sum+= arr[i];
+		}
+		System.out.println(sum/arr.length);
+	}
+
+}
