8e0ec13a6fb415a6de0e868092738430e05de2ed
apoorv-gthub <aal.apoorv@gmail.com>
2026-08-20 22:48:22 +0530

    Print all elements

---
+++ printAllElements.java
@@ -0,0 +1,15 @@
+package dsaArrays;
+
+public class printAllElements {
+
+	public static void main(String[] args) {
+		
+		int arr1[] = {11,22,33,44};
+		
+		for(int i = 0; i< arr1.length; i++) {
+			System.out.println(arr1[i]);
+		}
+		
+	}
+
+}
