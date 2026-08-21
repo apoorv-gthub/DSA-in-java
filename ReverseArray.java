20e6fbfc23d53c0d2dc9135f1231b773f1ae0cee
apoorv-gthub <aal.apoorv@gmail.com>
2026-08-21 22:51:43 +0530

    Reverse an array

---
+++ ReverseArray.java
@@ -0,0 +1,18 @@
+package dsaArrays;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {11,22,33,44,55};
+		
+		for(int i = 0; i < arr.length/2; i++) {
+			int temp = arr[i];
+			arr[i] = arr[arr.length-1-i];
+			arr[arr.length -1 -i] = temp;
+		}
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
